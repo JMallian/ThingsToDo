9fa3db0f21fc0c38b3f7d069f0c4abe102bdd705
Jessica Mallian <jlmallian@gmail.com>
2018-09-05 17:29:38 -0500

    added navigationItem to DisplayTasksViewController

---
+++ ThingsToDo/DisplayTasksViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DisplayTasksViewController.swift
+//  ThingsToDo
+//
+//  Created by Jessica Mallian on 9/5/18.
+//  Copyright © 2018 Jessica Mallian. All rights reserved.
+//
+
+import UIKit
+
+class DisplayTasksViewController: UITableViewController {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+    }
+    
+    @objc func addButtonTapped() {
+        //go to another ViewController to create a task
+    }
+}
