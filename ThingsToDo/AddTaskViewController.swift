6af7e75b32dabcc69e42d2e0f536376b47338e99
Jessica Mallian <jlmallian@gmail.com>
2018-09-07 18:54:58 -0500

    added more components to AddTaskViewController in storyboard and created connections

--- ThingsToDo/AddTaskViewController.swift
+++ ThingsToDo/AddTaskViewController.swift
@@ -5,29 +5,33 @@
 //  Created by Jessica Mallian on 9/5/18.
 //  Copyright © 2018 Jessica Mallian. All rights reserved.
 //
-//TODO: implement method to dismiss keyboard when Return is pressed
+
 //TODO: placeholder text in textfield that autoclears
 //TODO: add UITextField and UISwitch to fill out 3 fields of a tasks
 import UIKit
 class AddTaskViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var titleField: UITextField!
     @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var prioritySwitch: UISwitch!
+    @IBOutlet weak var descriptionTextView: UITextView!
     //var data: [Task]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.titleField.delegate = self
+        titleField.text = "Enter Title for Task"
     }
     
     @IBAction func addTaskButtonPressed(_ sender: Any) {
         guard let title = titleField.text, title != "" else {
-            displayLabel.text = "Please enter a title for your task."
-            print("text not entered")
+            displayLabel.text = "Please enter a title for your task." //will clear inside textFieldDidBeginEditing
             return
         }
         let newTask = Task(title: title)
         TasksStorage.storage.append(newTask)
-        print("storage now has \(TasksStorage.storage.count) items")
+        //hide keyboard and clear text as indicators that something happened when button pressed - could go back to list butuser may want to add more than 1 task at a time
+        titleField.resignFirstResponder()
+        titleField.text = ""
     }
     
     //MARK: UITextFieldDelegate methods
@@ -38,5 +42,6 @@ class AddTaskViewController: UIViewController, UITextFieldDelegate {
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
         textField.text = ""
+        displayLabel.text = ""
     }
 }
